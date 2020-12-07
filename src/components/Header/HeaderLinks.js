d5022a79ecbeaf1367863521fc47e89df3551778
Tristan <tristan@Air-de-Tristan.concorde-pereire.loc>
2020-12-07 14:49:34 +0100

    Home Page ready

---
+++ src/components/Header/HeaderLinks.js
@@ -0,0 +1,50 @@
+/*eslint-disable*/
+import React from "react";
+
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+
+// @material-ui/icons
+import PersonIcon from '@material-ui/icons/Person';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+
+// core components
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Button 
+          href="/login"
+          color="primary"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <PersonIcon className={classes.icons} /> Log in
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="/signup"
+          color="primary"
+          target="_blank"
+          border
+          className={classes.navLink}
+        >
+          <PersonAddIcon className={classes.icons} /> Sign Up
+        </Button>
+      </ListItem>
+    </List>
+  );
+}
