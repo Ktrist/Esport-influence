bbdbb9d4fd9cffdb257dbf616de87e2f38308401
Tristan <tristan@Air-de-Tristan.concorde-pereire.loc>
2020-12-17 11:04:11 +0100

    recup taf charles

--- src/views/Campagne/CreateCampaign.js
+++ src/views/Campagne/CreateCampaign.js
@@ -96,7 +96,7 @@ function CreateCampaign(props) {
   const [dateStart, setStartDate] = useState(new Date());
   const [dateEnd, setEndDate] = useState(new Date());
   const [redirect, setRedirect] = useState(false)
-  const [listErrorsCreateCampaign, setlistErrorsCreateCampaign] = useState([])
+
 
   var handleSubmitCampaign = async () => {
 
@@ -114,13 +114,9 @@ function CreateCampaign(props) {
       setRedirect(true)
       // console.log('boy.rcampaignSave', body.campaignSave)
     } else { setRedirect(false)
-      setlistErrorsCreateCampaign(body.error)
     }
   }
-  var tabErrorsCreateCampaign = listErrorsCreateCampaign.map((error,i) => {
-    return(<p>{error}</p>)
-    console.log('tabError',tabErrorsCreateCampaign )
-  })
+  
 
   const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
   setTimeout(function () {
@@ -315,8 +311,6 @@ function CreateCampaign(props) {
 
                     <div className={themeClasses.root} style={{ marginBottom: '80px' }}>
 
-                 
-
                       <input 
                       onChange ={(e) => console.log(e.target)}
                         accept="image/*"
@@ -329,13 +323,12 @@ function CreateCampaign(props) {
                       <label htmlFor="raised-button-file">
                         <Button variant="raised" component="span" className={classes.button}>
                           Upload
-                        </Button>
+                          </Button>
                       </label>
                     </div>
                   </CardBody>
-                  {tabErrorsCreateCampaign}
+
                   <CardFooter className={classes.cardFooter}>
-                  
                     <Button onClick={() => handleSubmitCampaign()} color="primary" size="lg">
                       Confirm
                       </Button>
@@ -347,7 +340,6 @@ function CreateCampaign(props) {
         </div>
       </div>
       {redirect ? <Redirect to="/choiceinfluencer" /> : null }
-      { tabErrorsCreateCampaign.length >0 ?  tabErrorsCreateCampaign : null }
     </div>
 
 
